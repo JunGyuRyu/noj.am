7710a5c5dc3c0b92c6dad2c958a4dc61177a02fa
JunGyuRyu <88473185+JunGyuRyu@users.noreply.github.com>
2024-01-04 08:55:40 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    
+    if(a>=90){printf("A\n");
+            }
+    else if(a>=80){printf("B\n");
+    }
+    else if(a>=70){printf("C\n");
+    }
+    else if(a>=60){printf("D\n");
+    }
+    else{
+        printf("F\n");
+    }
+}
