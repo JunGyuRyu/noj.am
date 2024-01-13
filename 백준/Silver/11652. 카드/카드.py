28bb1faa02c12ce7cb116882e2c873c8de7f2dfa
JunGyuRyu <88473185+JunGyuRyu@users.noreply.github.com>
2024-01-13 14:53:36 +0900

    [Silver IV] Title: 카드, Time: 4096 ms, Memory: 58112 KB -BaekjoonHub

---
+++ 백준/Silver/11652. 카드/카드.py
@@ -0,0 +1,13 @@
+from sys import stdin
+num_len = int(input())
+count_dict = dict()
+
+for i in range(num_len):
+    num = int(input());
+    if num not in count_dict:
+        count_dict[num] = 1
+    else:
+        count_dict[num] += 1
+
+sorted_dict = dict(sorted(count_dict.items()))
+print(sorted(sorted_dict.items(), key = lambda x: x[1], reverse=True)[0][0])
