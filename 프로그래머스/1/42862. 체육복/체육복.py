2582669ebcf95334ea82e2712e1f2ee9fb5af36b
JunGyuRyu <88473185+JunGyuRyu@users.noreply.github.com>
2024-01-22 23:03:19 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,22 +1,17 @@
 def solution(n, lost, reserve):
+    answer = n
     lost = sorted(lost)
     reserve = sorted(reserve)
+    # check_list = []
     
     # 중복 제거
     for i in range(1, n+1):
         if i in lost and i in reserve:
             lost.remove(i)
             reserve.remove(i)
-    
-    answer = n
-    check_list = []
-    
     for n in reserve:
-        if n-1 in lost and n not in check_list:
-            check_list.append(n-1)
+        if n-1 in lost:
             lost.remove(n-1)
-        elif n+1 in lost and n not in check_list:
-            check_list.append(n+1)
+        elif n+1 in lost:
             lost.remove(n+1)
-    print(lost, reserve)
     return answer - len(lost)
