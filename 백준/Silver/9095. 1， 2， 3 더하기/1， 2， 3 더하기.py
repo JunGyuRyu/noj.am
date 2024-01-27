fd940fdc91acf666f21c2eee1a9b51057def8df7
JunGyuRyu <88473185+JunGyuRyu@users.noreply.github.com>
2024-01-27 15:09:08 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -2,10 +2,8 @@ from sys import stdin
 T = int(input())
 a, b, c = 1, 2, 4
 l = [a, b, c]
-
 for i in range(11):
     l.append(l[-1]+l[-2]+l[-3])
-
 for i in range(T):
     n = int(input())
     print(l[n -1])
