48639bb909382c3a0316e71f87d32afa6adebfab
JunGyuRyu <88473185+JunGyuRyu@users.noreply.github.com>
2024-02-01 23:43:07 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 9532 ms, Memory: 31900 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -1,13 +1,17 @@
 from sys import stdin
+import gc
 
 N = int(stdin.readline())
 l = [0] * 100001
 
 for _ in range(N):
-    tmp = int(stdin.readline())
-    l[tmp] += 1
+    l[int(stdin.readline())] += 1
+
+del N
+gc.collect()
 
 for i, n in enumerate(l):
     if n != 0:
         for _ in range(n):
             print(i)
+            
