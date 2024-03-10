b00512b4656fa02cca223ebea3661d96d855f1a5
JunGyuRyu <88473185+JunGyuRyu@users.noreply.github.com>
2024-03-10 15:37:07 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.py
@@ -0,0 +1,9 @@
+from sys import stdin, stdout
+n = int(stdin.readline())
+for i in range(n):
+    stdout.write("*" * (i + 1) + " " * (((n - i - 2) * 2) + 2))
+    stdout.write("*" * (i + 1) + '\n')
+    
+for i in range(n):
+    stdout.write("*" * (n - i - 1) + " " * ((i * 2) + 2))
+    stdout.write("*" * (n - i - 1) + '\n')
